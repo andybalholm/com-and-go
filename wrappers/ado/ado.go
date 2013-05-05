81583df90038495ccd9f617acd6fa36d2e0daf7f
Andy Balholm <andy@balholm.com>
2013-05-04 18:10:03 -0700

    Start ADO wrapper.

---
+++ wrappers/ado/ado.go
@@ -0,0 +1,15 @@
+package ado
+
+import (
+	"com-and-go"
+)
+
+type Properties struct{ com.IDispatch } //TODO
+
+/* com
+// IID {00000534-0000-0010-8000-00AA006D2EA4}
+type _ADO interface {
+	com.IDispatch
+	Properties() (props *Properties, err error)
+}
+*/
